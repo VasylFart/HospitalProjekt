955aab69d8df0647944ee63194f9d8dfbf807434
Vasyl <vasylfart@gmail.com>
2023-01-10 21:23:32 +0100

    2

---
+++ server/src/core/V-Project.Application/Dtos/PeopleDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace V_Project.Application;
+
+public class PeopleDto
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int Age { get; set; }
+    public string? Country { get; set; }
+}
