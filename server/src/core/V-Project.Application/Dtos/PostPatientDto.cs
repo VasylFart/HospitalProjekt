54659c0345fe54045e76034b43b9509e607de7d0
Vasyl <vasylfart@gmail.com>
2023-01-23 20:03:35 +0100

    v1.3

--- server/src/core/V-Project.Application/Dtos/PostPatientDto.cs
+++ server/src/core/V-Project.Application/Dtos/PostPatientDto.cs
@@ -1,12 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
-
-namespace V_Project.Application;
+﻿namespace V_Project.Application;
 
 public class PostPatientDto
 {
     public string Name { get; set; }
-    public int Age { get => DateTime.Now.Year - DateOfBirth.Year; }
     public DateOnly DateOfBirth { get; set; }
     public string Pesel { get; set; }
     public string? City { get; set; }
