977ed635bccc8bb256754f0476ef99afc9c9b1bb
Vasyl <vasylfart@gmail.com>
2023-01-28 20:38:49 +0100

    v1.1

---
+++ server/tests/V-Project.Tests/DateOnlyTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using V_Project.Application;
+using Xunit;
+
+namespace V_Project.Tests
+{
+    public class DateOnlyTests
+    {
+
+        public static object[][] dateOfBirth =
+            {
+                new object[] {(1996,08,23), 26 },
+                new object[] {(1994,03,28), 28 },
+                new object[] {(1996,20,01), 27 }
+            };
+
+        [Theory]
+        [MemberData(nameof(dateOfBirth))]
+        public void CountAge_AddDateOfBirth_OldAreYou(DateOnly dateOfBirth, int age)
+        {
+            DateOnly dOnly = new DateOnly();
+
+            var result = dOnly.CountAge(dateOfBirth); 
+
+            Assert.Equal(age, result);
+
+        }
+    }
+}
